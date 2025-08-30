c4043ffe32ef889dcc771be4d5b17c4a860c7fa4
Vishesh <anandvh@mail.uc.edu>
2025-08-30 14:12:45 -0400

    bnhytfyg

--- src/utils/featureFlags.ts
+++ src/utils/featureFlags.ts
@@ -4,8 +4,5 @@ import { appConfig } from '../config/app';
  * Returns whether a given feature flag is enabled.
  * Added purely for webhook test commit.
  */
-export function isFeatureEnabled(flag: keyof typeof appConfig.features): boolean {
-  return Boolean(appConfig.features[flag]);
-}
 
 
