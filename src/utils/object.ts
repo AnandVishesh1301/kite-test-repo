f304fdb01e6417dad5c6e507549de86da04a6652
Vishesh <anandvh@mail.uc.edu>
2025-09-05 01:24:58 -0400

    commot graph test

---
+++ src/utils/object.ts
@@ -0,0 +1,25 @@
+/**
+ * Returns a new object with specified keys picked from the original object.
+ * Added for webhook testing.
+ */
+export function pick<T extends object, K extends keyof T>(obj: T, keys: K[]): Pick<T, K> {
+  const result = {} as Pick<T, K>;
+  for (const key of keys) {
+    if (key in obj) {
+      result[key] = obj[key];
+    }
+  }
+  return result;
+}
+
+/**
+ * Returns a new object with specified keys omitted from the original object.
+ * Added for webhook testing.
+ */
+export function omit<T extends object, K extends keyof T>(obj: T, keys: K[]): Omit<T, K> {
+  const result = { ...obj };
+  for (const key of keys) {
+    delete result[key];
+  }
+  return result;
+}
